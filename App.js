3cd988e6132a23a7eea489510a92e5c00cbbda93
Michael Cruz <mrcruz1989@gmail.com>
2021-06-06 22:29:03 -0400

    init complete. basica render and styling.

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.taskswrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+        <View style={styles.items}>
+          <Task text={'task 1'} />
+        </View>
+      </View >
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+
+  },
+  taskswrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+});
